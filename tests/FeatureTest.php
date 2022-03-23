a1294c73a90b0cf3cdbe830268d7c4d1eadd8467
Josh Grant <josh@synergitech.co.uk>
2022-03-23 13:38:26 +0000

    add tests to check that on-demand and notifiable notification sending works correctly

--- tests/FeatureTest.php
+++ tests/FeatureTest.php
@@ -5,12 +5,13 @@ namespace SynergiTech\Postal\Tests;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Notification;
 use Postal\Client;
+use Symfony\Component\Mailer\DelayedEnvelope;
 use SynergiTech\Postal\PostalNotificationChannel;
 use SynergiTech\Postal\PostalTransport;
 
 class FeatureTest extends TestCase
 {
-    public function testSendingNotificationLongForm()
+    public function testSendingNotificationOnDemand()
     {
         $result = new \stdClass;
         $result->message_id = 'feature-test';
@@ -29,7 +30,8 @@ class FeatureTest extends TestCase
         });
 
         $notifiable = new ExampleNotifiable();
-        Notification::route(PostalNotificationChannel::class, [$notifiable])
+
+        Notification::route(PostalNotificationChannel::class, 'feature-test@example.com')
             ->notify(new ExampleNotification($notifiable));
 
         $emailModel = config('postal.models.email');
@@ -44,7 +46,42 @@ class FeatureTest extends TestCase
         $this->assertSame('feature-test@example.com', $morph[0]->to_email);
     }
 
-    public function testSendingNotificationShortForm()
+    public function testSendingNotificationOnDemandWithAlias()
+    {
+        $result = new \stdClass;
+        $result->message_id = 'feature-test';
+        $message = new \stdClass();
+        $message->id = 'feature-test';
+        $message->token = 'feature-test';
+        $result->messages['feature-test@example.com'] = $message;
+
+        $clientMock = $this->createMock(Client::class);
+        $clientMock
+            ->method('makeRequest')
+            ->willReturn($result);
+
+        Mail::extend('postal', function (array $config = []) use ($clientMock) {
+            return new PostalTransport($clientMock);
+        });
+
+        $notifiable = new ExampleNotifiable();
+
+        Notification::route('postal', 'feature-test@example.com')
+            ->notify(new ExampleNotification($notifiable));
+
+        $emailModel = config('postal.models.email');
+        $email = $emailModel::where('to_email', 'feature-test@example.com')->first();
+        $this->assertNotNull($email);
+
+        $this->assertSame(ExampleNotifiable::class, $email->emailable_type);
+        $this->assertEquals($notifiable->id, $email->emailable_id);
+
+        $morph = $notifiable->emails()->get();
+        $this->assertCount(1, $morph);
+        $this->assertSame('feature-test@example.com', $morph[0]->to_email);
+    }
+
+    public function testSendingNotificationWithNotifiableTrait()
     {
         $result = new \stdClass;
         $result->message_id = 'feature-test';
@@ -75,5 +112,26 @@ class FeatureTest extends TestCase
         $morph = $notifiable->emails()->get();
         $this->assertCount(1, $morph);
         $this->assertSame('feature-test@example.com', $morph[0]->to_email);
+    }
+
+    public function testSendingNotificationWithNotifiableTraitWithoutRouteMethod()
+    {
+        $transportMock = \Mockery::mock(PostalTransport::class);
+        $transportMock->shouldReceive('send')
+            ->once()
+            ->withArgs(function ($email, DelayedEnvelope $envelope) {
+                $recipients = $envelope->getRecipients();
+                $this->assertCount(1, $recipients);
+
+                $this->assertSame('feature-test@example.com', $recipients[0]->getAddress());
+                return true;
+            });
+
+        Mail::extend('postal', function (array $config = []) use ($transportMock) {
+            return $transportMock;
+        });
+
+        $notifiable = new ExampleNotifiable();
+        $notifiable->notify(new ExampleNotification($notifiable));
     }
 }
